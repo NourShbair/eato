c881da3c8fc3949e2ffbca814da39be9ff8be1ca
Nour <nshbair@gmail.com>
2025-04-04 21:15:53 +0100

    Create simple view to display recipes

--- eato/urls.py
+++ eato/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from eato_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('recipes/', views.recipe_list, name='recipe_list'),
+
 ]
