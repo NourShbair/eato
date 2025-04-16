4808608db7e73b93f3b77dfd94c52544e811c05b
Nour <nshbair@gmail.com>
2025-04-16 11:17:29 +0100

    Add filter to recipes for user statistics

--- eato_app/views/recipes_list.py
+++ eato_app/views/recipes_list.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from ..models import Recipe
+from django.contrib.auth.decorators import login_required
 
+# @login_required
 def recipes_list(request):
-    recipes = Recipe.objects.all().order_by('-created_at')
-    paginator = Paginator(recipes, 6)  # Show 6 recipes per page
+    filter_by = request.GET.get('filter')
+    recipes = Recipe.objects.all()
+
+    if filter_by == 'mine':
+        recipes = recipes.filter(created_by=request.user)
+    elif filter_by == 'liked':
+        recipes = request.user.liked_recipes.all()
+    elif filter_by == 'saved':
+        recipes = request.user.favorite_recipes.all()
+
+    paginator = Paginator(recipes, 9)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'recipes/recipes_list.html', {
-        'page_obj': page_obj
-    })
+    return render(request, 'recipes/recipes_list.html', {'page_obj': page_obj})
